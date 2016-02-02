b1044dc20202cc49c7840939e88b08e577bd049b
Zopherus <1012746@IHS60261436.ad.nsd.org>
2016-02-02 13:08:45 -0800

    Created test data, read data from textfiles

---
+++ Stable Roommates/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grouping
+{
+	class Person
+	{
+		public string Name;
+		// Ordered list of preferences
+		public List<Person> Preferences = new List<Person>();
+		public Person(string Name)
+		{
+			this.Name = Name;
+		}
+	}
+}
---
+++ Stable Roommates/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Grouping
+{
+	class Program
+	{
+		// Number of preferences that each person submits
+		const int NUM_PREFERENCES = 5;
+		// Size of each room
+		const int SIZEROOM = 4;
+		static List<Person> People = new List<Person>();
+		static void Main(string[] args)
+		{
+			StreamReader streamReader = new StreamReader("C:/Users/1012746/Desktop/People.txt");
+			int numberOfPeople = int.Parse(streamReader.ReadLine());
+			for (int counter = 0; counter < numberOfPeople; counter++)
+			{
+				Person person = new Person(streamReader.ReadLine());
+				People.Add(person);
+			}
+
+			streamReader = new StreamReader("C:/Users/1012746/Desktop/Preferences.txt");
+			for (int personNumber = 0; personNumber < People.Count; personNumber++)
+			{
+				Person person = findPerson(streamReader.ReadLine());
+				for (int counter = 0; counter < NUM_PREFERENCES; counter++)
+				{
+					string name = streamReader.ReadLine().Split('.')[1];
+					person.Preferences.Add(findPerson(name));
+				}
+			}
+
+
+
+		}
+
+		static Person findPerson(string name)
+		{
+			foreach (Person person in People)
+			{
+				if (person.Name == name)
+					return person;
+			}
+			return null;
+		}
+
+	}
+}
